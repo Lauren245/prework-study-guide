023a52ab7bc3594a035de473e7fcf2f9f8ca3fb2
Lauren245 <102630328+Lauren245@users.noreply.github.com>
2024-09-25 17:31:01 -0700

    add JavaScript file with for loop and connect/link it to HTML file using script tag.

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for(let x = 0; x < topics.length; x++) {
+
+    console.log(topics[x]);
+    //optional version with concatenation to match previous if/else version's output.
+        //console.log("Let's study " + topics[x] + "!");  
+
+}
+
+/*
+if(topic === 'HTML') {
+
+    console.log("Let's study HTML!");
+
+} else if (topic === 'CSS') {
+
+    console.log("Let's study CSS!");
+
+} else if (topic === 'Git') {
+
+    console.log("Let's study Git!");
+
+} else if (topic === 'JavaScript') {
+
+    console.log("Let's study JavaScript!");
+
+} else {
+    
+    console.log('Please try again!');
+}
+*/
